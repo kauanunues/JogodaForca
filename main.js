71dbacce4898d758f37324f88b266ad45b186c10
kauanunues <marykakinho@gmail.com>
2024-10-10 12:24:42 -0300

    comit

---
+++ main.js
@@ -0,0 +1,5 @@
+const palavras = ["baiano","bahia","zédarede","zénatal"];
+let tentativasUsadas = 0;
+let letras = ['A','B','C','D','E','F','G','H','I','J','I','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',]
+let palavrasSecreta,palavraOculta;
+let jogando;
